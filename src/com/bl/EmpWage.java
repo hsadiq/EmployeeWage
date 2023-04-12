43967644e4093a880b038edaeca82ed8c63ea588
SADIQUE HUSSAIN <44770680+hsadiq@users.noreply.github.com>
2023-04-12 11:46:28 +0530

    Added Calculating Wage Code to EmployeeWage

--- src/com/bl/EmpWage.java
+++ src/com/bl/EmpWage.java
@@ -4,13 +4,21 @@ public class EmpWage {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation");
-
+        //Employee Attendance UC-1
         int att = (int)(Math.random() * 10) % 2;
         if (att==1){
             System.out.println("Employee is Present");
         }else{
             System.out.println("Employee is Absent");
         }
+        //Calculating Wage Per Hour UC-2
+
+        int hour_wage= 20;
+        int hours_day=8;
+
+        int wage = (hour_wage * hours_day);
+
+        System.out.println("Employee Daily wage is: "+ wage);
 
     }
 }
