2386e6aad5785b15f66e87535e5042c408e024eb
SADIQUE HUSSAIN <44770680+hsadiq@users.noreply.github.com>
2023-04-12 12:21:49 +0530

    Added the code to chech employee Attendenc

--- src/com/bl/EmpWage.java
+++ src/com/bl/EmpWage.java
@@ -14,11 +14,25 @@ public class EmpWage {
         //Calculating Wage Per Hour UC-2
 
         int hour_wage= 20;
-        int hours_day=8;
+        int full_day=8;
 
-        int wage = (hour_wage * hours_day);
+        int wage = (hour_wage * full_day);
 
         System.out.println("Employee Daily wage is: "+ wage);
+
+        //Calculating PartTime UC-3
+
+        int  half_day= 4;
+
+        int wage1= (hour_wage * half_day);
+
+        switch (wage){
+            case 1:
+                System.out.println("Employee full time wage is: "+ wage);
+
+            case 2:
+                System.out.println("Employee Part Time Wage is: "+ wage1);
+        }
 
     }
 }
