700d8690fd0dc2ef5442b7d9f708624ef4a4cf79
SADIQUE HUSSAIN <44770680+hsadiq@users.noreply.github.com>
2023-04-13 16:00:39 +0530

    Added the Switch Condition to the Code

--- src/com/bl/EmpWage.java
+++ src/com/bl/EmpWage.java
@@ -1,9 +1,17 @@
 package com.bl;
 
+import java.util.Scanner;
+
 public class EmpWage {
+
+    public static final int Part_time =1;
+    public static final int Full_time =2;
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation");
+
+
         //Employee Attendance UC-1
         int att = (int)(Math.random() * 10) % 2;
         if (att==1){
@@ -11,6 +19,12 @@ public class EmpWage {
         }else{
             System.out.println("Employee is Absent");
         }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your work hour: ");
+        int ent = sc.nextInt();
+
+
         //Calculating Wage Per Hour UC-2
 
         int hour_wage= 20;
@@ -22,17 +36,25 @@ public class EmpWage {
 
         //Calculating PartTime UC-3
 
-        int  half_day= 4;
 
-        int wage1= (hour_wage * half_day);
 
-        switch (wage){
-            case 1:
-                System.out.println("Employee full time wage is: "+ wage);
+        int Emp_Hour=0;
+        int Emp_wage=0;
 
-            case 2:
-                System.out.println("Employee Part Time Wage is: "+ wage1);
+        int EmpCheck =(int) Math.floor(Math.random()* 10) % 3;
+
+        switch (EmpCheck){
+            case Part_time:
+                Emp_Hour = 4;
+                break;
+            case Full_time:
+                Emp_Hour = 8;
+                break;
+            default:
+                Emp_Hour = 0;
         }
+        Emp_wage = Emp_Hour * hour_wage;
+        System.out.println("Your Wage: "+Emp_wage);
 
 
 
